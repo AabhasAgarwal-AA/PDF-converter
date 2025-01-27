8c5d7b7f9960148ba9b49acea4234cb73d95a1bd
AabhasAgarwal-AA <ag7152@srmist.edu.in>
2025-01-27 22:28:43 +0530

    backend completed

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const multer = require("multer");
+const docxConverter = require('docx-pdf');
+const path = require("path");
+
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) { 
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+app.post('/convertFile', upload.single('file'), function (req, res, next) {
+    try{
+        if(!req.file){
+            return res.status(400).json({
+                message: "No file was uploaded"
+            });
+        }
+        let outputPath = path.join(__dirname, "files", `${req.file.originalname}.pdf`);
+        docxConverter(req.file.path, outputPath, function (err, result) {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    message: "error converting docx to pdf"
+                });
+            }
+            res.download(outputPath, () => {
+                console.log("file downloaded");
+            });
+        });
+
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({
+            message: "internal server error"
+        });
+    }
+});
+
+
+app.listen(3000);
