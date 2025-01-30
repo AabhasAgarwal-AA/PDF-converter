7060adaa526eb831d339f9826f1fbbb80153b1b7
AabhasAgarwal-AA <ag7152@srmist.edu.in>
2025-01-30 21:29:03 +0530

    fe created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { Footer } from './components/Footer'
+import { Home } from './components/Home'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
-    <div className='bg-blue-500'>
-      test
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   )
 }
 
